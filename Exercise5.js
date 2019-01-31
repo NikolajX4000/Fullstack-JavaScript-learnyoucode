4e2612b230294e2d740959a4ba7f1239b5d464a9
NikolajX4000 <supernikolaj@hotmail.com>
2019-01-31 14:44:28 +0100

    exercise6 complete

--- Exercise5.js
+++ Exercise5.js
@@ -5,5 +5,5 @@ fs.readdir(process.argv[2], (err, list) => {
     if (err) {
         throw new Error(err)
     }
-    list.filter(x => path.extname(x) == `.${process.argv[3]}`).forEach(x => console.log(x))
+    list.filter(x => path.extname(x) === `.${process.argv[3]}`).forEach(x => console.log(x))
 })
